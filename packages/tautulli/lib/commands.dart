05d1af6f141fcb96fdbc0db0d937a3818ff77835
Jagandeep Brar <jagandeep.brar@solarskyrise.com>
2020-07-08 15:17:05 -0500

    Implemented terminate_session and restart

--- packages/tautulli/lib/commands.dart
+++ packages/tautulli/lib/commands.dart
@@ -2,15 +2,18 @@
 library tautulli_commands;
 
 import 'package:dio/dio.dart';
+import 'package:meta/meta.dart';
 import './models.dart';
 
 // Activity
 part './src/commands/activity.dart';
 part './src/commands/activity/get_activity.dart';
+part './src/commands/activity/terminate_session.dart';
 // Miscellaneous
 part './src/commands/miscellaneous.dart';
 part './src/commands/miscellaneous/arnold.dart';
 // System
 part './src/commands/system.dart';
+part './src/commands/system/restart.dart';
 part './src/commands/system/update.dart';
 part './src/commands/system/update_check.dart';
